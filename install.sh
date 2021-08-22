98b32e5162b75b07eb1b55338462d381b36073cd
casjay <git-admin@casjaysdev.com>
2021-08-22 11:38:55 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -82,7 +82,7 @@ if_os mac && APP="curl wget lynx links nano locate "
 if_os linux && APP="curl wget lynx pip3 xdotool links nano ibus mlocate "
 PERL=""
 PYTH="pip setuptools "
-PIPS="shodan ytmdl asciinema toot tootstream rainbowstream irc "
+PIPS="shodan ytmdl asciinema toot tootstream rainbowstream irc virtualenvwrapper "
 CPAN=""
 GEMS=""
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
