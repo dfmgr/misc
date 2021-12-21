b17c7bac9c4cd775e3682c75c7761d5be8baadb4
Jason <git-admin@casjaysdev.com>
2021-12-21 01:12:52 -0500

    🗃 Modified: etc/shell/aliases/00-aliases.sh  🗃

--- etc/shell/aliases/00-aliases.sh
+++ etc/shell/aliases/00-aliases.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version       : 202103251632-git
+# @Author        : Jason Hempstead
+# @Contact       : jason@casjaysdev.com
+# @License       : LICENSE.md
+# @ReadME        : README.md
+# @Copyright     : Copyright: (c) 2021 Jason Hempstead, CasjaysDev
+# @Created       : Thursday, Mar 25, 2021 16:36 EDT
+# @File          : 00-aliases.lin
+# @Description   : Aliases for all OSes
+# @TODO          :
+# @Other         :
+# @Resource      :
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Aliases for all OSes
+[[ -f "$(builtin type -P minikube 2>/dev/null)" ]] && alias kubectl='minikube kubectl'
